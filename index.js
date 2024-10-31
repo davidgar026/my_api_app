1047b19a977de023d60343038a7932c7990f07c7
davidgar026 <130368930+davidgar026@users.noreply.github.com>
2024-10-30 23:10:50 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import ejs from "ejs";
 
 const app = express();
 const port = 3000;
-const API_url = "http://taco-randomizer.herokuapp.com/";
+const API_url = "https://api.tvmaze.com/shows";
 const body = {
 
 };
@@ -17,9 +17,12 @@ app.use(express.static("public"));
 
 app.get("/", async(req, res) => {
     try{
-        const result = await axios.get(API_url + "random");
+        const result = await axios.get(API_url);
         res.render("index.ejs", {
-            content:JSON.stringify(result.data),
+            tvshow: JSON.stringify((result.data[0].name)).replace(/['"]+/g, ''),
+            year: JSON.stringify((result.data[0].premiered)).replace(/['"]+|-.*/g, ''),
+            genre: JSON.stringify((result.data[0].genres)).replace(/['"]+/g, ''),
+
         });
     }catch(error){
         res.status(404).send(error.message);
