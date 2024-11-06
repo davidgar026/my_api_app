a99bc369aac8e543f72447d59df276369d33ab21
davidgar026 <130368930+davidgar026@users.noreply.github.com>
2024-11-05 22:07:43 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-import ejs from "ejs";
 
 const app = express();
 const port = 3000;
@@ -15,19 +14,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-app.get("/", async(req, res) => {
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+
+
+
+
+app.post("/", async(req, res) => {
     try{
-        const result = await axios.get(API_url);
-        res.render("index.ejs", {
+        const result = await axios.get(API_url, req.body,{});
+        res.render("results.ejs", {
+            yourName: req.body.nameText,
+            yourBirthYear: req.body.birthYear,
+            yourGenre: req.body.favGenre,
             tvshow: JSON.stringify((result.data[0].name)).replace(/['"]+/g, ''),
             year: JSON.stringify((result.data[0].premiered)).replace(/['"]+|-.*/g, ''),
             genre: JSON.stringify((result.data[0].genres)).replace(/['"]+/g, ''),
-
-        });
+        })
     }catch(error){
         res.status(404).send(error.message);
     }
-})
+});
 
 
 
