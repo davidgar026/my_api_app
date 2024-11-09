bff6e6d2dd37de67da595c7467956000b9724a74
davidgar026 <130368930+davidgar026@users.noreply.github.com>
2024-11-09 00:08:49 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -19,16 +19,40 @@ app.get("/", (req, res) => {
 })
 
 
-
-
-
 app.post("/", async(req, res) => {
     try{
         const result = await axios.get(API_url, req.body,{});
+        const selectedOptions = req.body.options;
+            //console.log(selectedOptions[1])
+        if (Array.isArray(selectedOptions)) {
+            console.log("Selected options:", selectedOptions);
+          } else if (selectedOptions) {
+            // If only one checkbox is selected, it will be a string
+            console.log("Selected option:", [selectedOptions]);
+          } else {
+            console.log("No options selected.");
+          }
+
+
+          result.data.forEach(el => {
+            //console.log(el.name)
+        })
+
+
+          
+        //console.log(selectedOptions.includes(result.data[0].genres))
+        //console.log("selectedOptions = ", typeof(selectedOptions));
+        //const hasValue = (obj, value) => Object.values(obj).includes(value);
+        //console.log("Obj = ", result.data[0])
+        //console.log("true or false: ", hasValue(result.data[0],'Ended'))
+        console.log("true or false: ", result.data[0].genres.includes('Science-Fiction'))
+
+
+
         res.render("results.ejs", {
             yourName: req.body.nameText,
             yourBirthYear: req.body.birthYear,
-            yourGenre: req.body.favGenre,
+            yourGenre: selectedOptions,
             tvshow: JSON.stringify((result.data[0].name)).replace(/['"]+/g, ''),
             year: JSON.stringify((result.data[0].premiered)).replace(/['"]+|-.*/g, ''),
             genre: JSON.stringify((result.data[0].genres)).replace(/['"]+/g, ''),
@@ -37,6 +61,8 @@ app.post("/", async(req, res) => {
         res.status(404).send(error.message);
     }
 });
+
+
 
 
 
