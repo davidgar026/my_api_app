bcb828d68469f04a55b2df727ee68f8ab3442fde
davidgar026 <130368930+davidgar026@users.noreply.github.com>
2024-11-18 17:02:23 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 const API_url = "https://api.tvmaze.com/shows";
-const filteredTvShowsByGenre = [];
-const filteredTvShowsByPremieredWithZeroTol = [];
-const filteredTvShowsByPremieredWithNonZeroTol = [];
-let finalFilteredTvShow = [];
-let finalFilteredTvShow2 = [];
+//let finalFilteredTvShow2 = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -23,6 +19,8 @@ app.get("/", (req, res) => {
 app.post("/", async(req, res) => {
 
     //Functions
+
+    //capitalizes the first letter of each string in a array
     function capitalizeFirstLetterInArray(arr) {
         return arr.map(text => {
           return text.replace(/\b[a-zA-Z]/g, function(match) {
@@ -31,6 +29,7 @@ app.post("/", async(req, res) => {
         });
       }
 
+      //gets a random number between two inclusive numbers
       function getRandomIntInclusive(min, max) {
         const minCeiled = Math.ceil(min);
         const maxFloored = Math.floor(max);
@@ -39,10 +38,14 @@ app.post("/", async(req, res) => {
 
 
 
-
+      //async function
     try{
         const result = await axios.get(API_url, req.body,{});
         const selectedOptions = req.body.options;
+        let filteredTvShowsByGenre = [];
+        let filteredTvShowsByPremieredWithZeroTol = [];
+        let filteredTvShowsByPremieredWithNonZeroTol = [];
+        let finalFilteredTvShow = [];
 
        
 
@@ -75,37 +78,78 @@ app.post("/", async(req, res) => {
             });
 
            
-            /*YOU LEFT OFF HERE 
-        
-            (11/17/24 9:22pm)
-        
-            What tf was I doing: I was trying I was trying to allow a user to input their birth year that does not exist in the api's array of tv shows. I was able to use the absolute and use tolerance to round the user's birth year input to the nearest premier date of a tv show. 
-
-            What's next: do more tests. update the results.ejs page once all is confirmed.
-        
-        
-        */
-            console.log("true or false: ", filteredTvShowsByPremieredWithZeroTol != '');
+            
            
 
             if(filteredTvShowsByPremieredWithZeroTol != ''){
                 finalFilteredTvShow = filteredTvShowsByPremieredWithZeroTol[getRandomIntInclusive(0,filteredTvShowsByPremieredWithZeroTol.length-1)];
-                console.log("End Result = ", finalFilteredTvShow);
+                //console.log("End Result = ", finalFilteredTvShow);
             }else{
-                finalFilteredTvShow2 = filteredTvShowsByPremieredWithNonZeroTol[getRandomIntInclusive(0,filteredTvShowsByPremieredWithNonZeroTol.length-1)];
-                console.log("End Result = ", finalFilteredTvShow2);
+                finalFilteredTvShow = filteredTvShowsByPremieredWithNonZeroTol[getRandomIntInclusive(0,filteredTvShowsByPremieredWithNonZeroTol.length-1)];
+                //console.log("End Result = ", finalFilteredTvShow);
             }
             
 
-            /* you left off here => */
             
 
 
 
           } else if (selectedOptions) {
             // If only one checkbox is selected, it will initially be a string but below, the REGEX will turn it into a obj.
-            const turnStringToObj = selectedOptions.replace(/\b(\w+)\b/g, '[$1]');
-            console.log("Selected option:", turnStringToObj)
+            //const turnStringToObj = selectedOptions.replace(/\b(\w+)\b/g, '[$1]');
+
+              console.log("selected = ", selectedOptions);
+
+              
+            for(let i=0;i<result.data.length; i++){
+                let availableApiGenres = result.data[i].genres;
+                let oneGenreUserSelection = selectedOptions.replace(/\b[a-zA-Z]/g, function(match) {
+                    return match.toUpperCase();
+                  });
+                let hasMatch = availableApiGenres.includes(oneGenreUserSelection);
+                if(hasMatch){
+                    filteredTvShowsByGenre.push(result.data[i])
+                }
+            }
+
+
+            /*YOU LEFT OFF HERE 
+        
+            (11/18/24 5pm)
+        
+            What tf was I doing: I finished coding for when a user inputs multiple genres. Now I'm coding for when the user inputs one selected genre. As of now I'm just confirming that it works. I tried doing a birth year input of 2005 and console logging all the options available as shown in the console.
+
+            What's next: do more tests. update the results.ejs page once all is confirmed.
+        
+        
+        */
+
+            /*  You left off here ------> */filteredTvShowsByGenre.forEach(el => {
+                console.log('id: ', el.id, ", year: ", el.premiered , ", genres: ", el.genres);
+            })
+
+            if(filteredTvShowsByGenre != ''){
+            filteredTvShowsByGenre.forEach(el => {
+              //filters each object item's premier data
+              if(Math.abs(req.body.birthYear - el.premiered.replace(/['"]+|-.*/g, '')) == 0){
+                filteredTvShowsByPremieredWithZeroTol.push(el)
+              }
+            });
+
+            console.log("filteredTvShowsByPremieredWithZeroTol = ", filteredTvShowsByPremieredWithZeroTol );
+
+            //after filtering by the tv show's premier year with user's birth year, finally it filters by available premier year based off of user's input. 
+            if(filteredTvShowsByPremieredWithZeroTol != ''){ //if the user's birth year exists in the API
+                finalFilteredTvShow = filteredTvShowsByPremieredWithZeroTol[getRandomIntInclusive(0,filteredTvShowsByPremieredWithZeroTol.length-1)];
+            }
+
+        }else{
+            console.log("The given input for birth year has no associated shows.")
+        }
+
+            //console.log("End result = ", finalFilteredTvShow)
+
+            
             
           } else {
             console.log("No options selected.");
@@ -117,9 +161,9 @@ app.post("/", async(req, res) => {
             yourName: req.body.nameText,
             yourBirthYear: req.body.birthYear,
             yourGenre: selectedOptions,
-            tvshow: JSON.stringify((result.data[0].name)).replace(/['"]+/g, ''),
-            year: JSON.stringify((result.data[0].premiered)).replace(/['"]+|-.*/g, ''),
-            genre: JSON.stringify((result.data[0].genres)).replace(/['"]+/g, ''),
+            tvshow: finalFilteredTvShow.name,
+            year: finalFilteredTvShow.premiered,
+            genre: finalFilteredTvShow.genres,
         })
     }catch(error){
         res.status(404).send(error.message);
